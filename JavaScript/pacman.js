cc08d761af5fe0f2a7c83af5fd287bc7354e96dd
killshot13 <dmreh@outlook.com>
2020-06-26 09:42:17 -0500

    update js files, created css & linked, rendered on canvas

--- JavaScript/pacman.js
+++ JavaScript/pacman.js
@@ -1,64 +1,123 @@
-/*var map = [
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,],
-    [1,2,2,2,2,2,1,2,2,2,2,2,1,],
-    [1,2,1,1,1,2,1,2,1,1,1,2,1,],
-    [1,2,1,2,2,2,2,2,2,2,1,2,1,],
-    [1,2,2,2,1,1,5,1,1,2,2,2,1,],
-    [1,2,1,2,2,2,2,2,2,2,1,2,1,],
-    [1,2,1,1,1,2,1,2,1,1,1,2,1,],
-    [1,2,2,2,2,2,1,2,2,2,2,2,1,],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,],
-]
-var pacman = {
-    x: 6,
-    y: 4
-}
+let canvas = document.getElementById('gameCanvas')
+let ctx = canvas.getContext('2d')
 
-function drawWorld() {
-    for(var y = 0; y < map.length; y++){
-        for(var x = 0; x < map.length; x++){
-            if (map[y][x] === 1) {
-                document.getElementById('world').innerHTML += "<div class='wall'></div>";
-            }
-            else if (map[y][x] === 2) {
-                document.getElementById('world').innerHTML += "<div class='coin'></div>";
-            }
-            else if (map[y][x] === 3) {
-                document.getElementById('world').innerHTML += "<div class='ground'></div>";
-            }
-            else if (map[y][x] === 5) {
-                document.getElementById('world').innerHTML += "<div class='pacman'></div>";
-            }
+let gameData = [
+    [1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,2,2,2,2,2,1,2,2,2,2,2,1],
+    [1,2,1,1,1,2,1,2,1,1,1,2,1],
+    [1,2,1,2,2,2,2,2,2,2,1,2,1],
+    [1,2,2,2,1,1,5,1,1,2,2,2,1],
+    [1,2,1,2,2,2,2,2,2,2,1,2,1],
+    [1,2,1,1,2,2,1,2,2,1,1,2,1],
+    [1,2,2,2,2,2,1,2,2,2,2,2,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1]
+];
 
+const wall = 1;
+const coin = 2;
+const ground = 3;
+const pacman = 5;
+
+let map;
+let pacman = {
+x: 6,
+y: 4,
+direction: 'right'
+};
+function createTiles(data) {
+    let tilesArray = [];
+    for (let row of data) {
+        for (let col of row) {
+            let tile = document.createElement('div');
+            tile.classList.add('tile');
+                if (col == wall) {
+                    tile.classList.add('wall');
+                } else if (col == coin) {
+                    tile.classList.add('coin');
+                } else if (col == ground) {
+                    tile.classList.add('ground');
+                } else if (col == pacman) {
+                    tile.classList.add('pacman');
+            tile.classList.add(pacman.direction);
+            }
+            tilesArray.push(tile);
         }
-        docment.getElementById('world').innerHTML += "<br>";
+        let brTile = document.createElement('br');
+        tilesArray.push(brTile);
+    }
+    return tilesArray;
+}
+
+function drawMap() {
+    map = document.createElement('div');
+    let tiles = createTiles(gameData);
+    for (let tile of tiles) {
+        map.appendChild(tile);
+    }
+    document.body.appendChild(map);
+}
+
+function eraseMap() {
+    document.body.removeChild(map);
+}
+
+function moveDown() {
+    pacman.direction = 'down';
+    if (gameData[pacman.y+1][pacman.x] !== wall) {
+        gameData[pacman.y][pacman.x] = ground;
+        pacman.y = pacman.y + 1 ;
+        gameData[pacman.y][pacman.x] = pacman;
     }
 }
 
-document.onkeydown = function(e){
-    if (e.keyCode === 37) {
-        map[pacman.y][pacman.x] = 3;
+function moveUp() {
+    pacman.direction = 'up';
+    if (gameData[pacman.y-1][pacman.x] !== wall) {
+        gameData[pacman.y][pacman.x] = ground;
         pacman.y = pacman.y - 1;
-        map[pacman.y][pacman.x] = 5;
-    }
-    else if (e.keyCode === 38) {
-        if (map[pacman.y - 1][pacman.x] !== 1)
-            map[pacman.y][pacman.x] = 3;
-        pacman.y = pacman.y - 1;
-        map[pacman.y][pacman.x] = 5;
-    }
-    else if (e.keyCode === 39) {
-        map[pacman.y][pacman.x] = 3;
-        pacman.y = pacman.y + 1;
-        map[pacman.y][pacman.x] = 5;
-    }
-    else if (e.keyCode === 40) {
-        if (map[pacman.y + 1][pacman.x] !== 1)
-            map[pacman.y][pacman.x] = 3;
-        pacman.y = pacman.y - 1;
-        map[pacman.y][pacman.x] = 5;
+        gameData[pacman.y][pacman.x] = pacman;
     }
 }
-drawWorld();
+
+function moveLeft() {
+    pacman.direction = 'left';
+    if (gameData[pacman.y][pacman.x-1] !== wall) {
+        gameData[pacman.y][pacman.x] = ground;
+        pacman.x = pacman.x - 1 ;
+        gameData[pacman.y][pacman.x] = pacman;
+    }
+}
+
+function moveRight() {
+    pacman.direction = 'right';
+    if (gameData[pacman.y][pacman.x+1] !== wall) {
+        gameData[pacman.y][pacman.x] = ground;
+        pacman.x = pacman.x + 1 ;
+        gameData[pacman.y][pacman.x] = pacman;
+    }
+}
+
+function setupKeyboardControls() {
+    document.addEventListener('keydown', function (e) {
+        if (e.keyCode === 37) {        
+            moveLeft();
+        } else if (e.keyCode === 38) {  
+            moveUp();
+        } else if (e.keyCode === 39) {   
+            moveRight();
+        } else if (e.keyCode === 40) {  
+            moveDown();
+        }
+    eraseMap();
+    drawMap();
+    });
+}
+
+function startGame() {
+    drawMap();
+    setupKeyboardControls();
+}
+
+startGame();
 
 
